2881c83518381dab38c3c3afd93dae7a98864f82
BetterCallAle <dap22091998@gmail.com>
2022-12-14 15:24:11 +0100

    you can switch between two pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name("home");
+
+
+Route::get('/mars', function(){
+    return view('mars');
+})->name('mars');
